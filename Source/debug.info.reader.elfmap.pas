00e61fa3a8cf01b834fa41e213924e9c530169ff
Jim McKeeth <jim@mckeeth.org>
2026-04-07 02:08:43 -0600

    remove copyright

--- Source/debug.info.reader.elfmap.pas
+++ Source/debug.info.reader.elfmap.pas
@@ -1,11 +1,10 @@
 unit debug.info.reader.elfmap;
 
 (*
- * Copyright (c) 2021 Anders Melander
+ * Public Domain - No copywright
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This is AI generated code and not subject to copyright.
+ *
  *)
 
 interface
